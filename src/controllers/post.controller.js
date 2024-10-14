9890cad58b461c31060018f38d4efabb75da089e
Alex Kozhukhar <akozhukhar@atncorp.com>
2024-10-14 13:04:18 +0300

    add erro http status

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,3 +1,5 @@
+const httpStatus = require('http-status');
+const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 const { postService } = require('../services');
 
@@ -8,6 +10,9 @@ const getAllPosts = catchAsync(async (req, res) => {
 
 const getPostById = catchAsync(async (req, res) => {
   const post = await postService.queryPostById(req.params.postId);
+  if(!post) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Post not found');
+  }
   res.send(post);
 });
 
